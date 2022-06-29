68ebb6df4aaa2182c6dd948a34d8b5c10f674d6c
Hector <89035945+hectoresen@users.noreply.github.com>
2022-06-29 18:21:30 +0200

    FEAT: Cleaning index and adding description

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { HackBgProvider } from './Shared/Contexts/BgContext';
 import LanguageWrapper from './Shared/Contexts/LanguageContext';
-import english from './lang/en.json';
-import spanish from './lang/es.json';
 import './index.scss';
 
 const container = document.getElementById("root");
